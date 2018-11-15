1e733187fdef44a72eaef55e3a50a3babbf53479
xiaoweige <469958860@qq.com>
2018-11-15 11:54:54 +0800

    打款哈萨克的建行卡

--- sale/models.py
+++ sale/models.py
@@ -24,7 +24,7 @@ class Typeinfo(models.Model):
 
 class Carinfo(models.Model):
     name = models.CharField('安静的就看到',max_length=30,null=False)
-    date = models.DateField('上牌日期',null=False)
+    date = models.IntegerField('大声',null=False)
     fadongnum = models.CharField('发动机号',max_length=20)
     mile = models.DecimalField('公里数',max_digits=7,decimal_places=2)
     isweixiu = models.BooleanField('是否维修',default=False)
